6f41c6ecfb962d5403f66bd7840be49ae1c53730
Mitchell Middleton <mitch.middleton18@gmail.com>
2024-11-04 15:22:11 -0800

    Fixed bug that allowed a game note voted on to pass

--- GameSelection.py
+++ GameSelection.py
@@ -11,6 +11,7 @@ def pick_game(votes: np.ndarray, times_played: np.ndarray) -> int:
     if np.isnan(proportinal_times_played).any():
         return np.argmax(proportional_votes)
     diff = proportional_votes - proportinal_times_played
+    diff = remove_non_voted_games(diff, votes[-1,:])
     return np.argmax(diff)
 
 def get_accum(array: np.ndarray) -> np.ndarray:
@@ -18,4 +19,26 @@ def get_accum(array: np.ndarray) -> np.ndarray:
     for row in array:
         accum = FLEETING_INTEREST*accum + row
     return accum
+
+def remove_non_voted_games(diff: np.ndarray, latest_vote: np.ndarray) -> np.ndarray:
+    destroying_array = []
+    for vote in latest_vote:
+        if vote <= 0:
+            destroying_array.append(-1000)
+        else:
+            destroying_array.append(1)
+    return diff + np.array(destroying_array)
+
+if __name__ == '__main__':
+    votes = np.array([
+        [0,1,2,100,4,5],
+        [0,1,2,0,4,5]
+    ])
+    times_played = np.array([
+        [0,0,0,0,0,1],
+        [0,0,0,0,1,0]
+    ])
+
+    game_idx = pick_game(votes, times_played)
+    print(game_idx)
     
